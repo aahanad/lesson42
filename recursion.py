8aa40ef2200ba1db1373ed8e828845d11b6aba1a
aahanad <150878761+aahanad@users.noreply.github.com>
2025-06-30 18:55:40 -0700

    Add files via upload

---
+++ recursion.py
@@ -0,0 +1,32 @@
+#n=input("What number?")
+#def sum(n):
+#    if n==1:
+#        return n
+#    else:
+#        return sum(n-1)+n
+#print(sum(4))
+#sum(4)
+
+#factorials
+#n=input("what number")
+#def sum(n):
+#    if n==1:
+#        return n
+#    else:
+#        return n* sum (n-1)
+#print (sum(3))
+#sum(3)
+
+#exponents
+n=int(input("what number?"))
+e=int(input("what exponent?"))
+def sum(n,e):
+    if e==1:
+        return n
+    elif e==0:
+        return 1 
+    else:
+        return n* sum (n,e-1)
+print (sum(n,e))
+sum(n,e)
+
